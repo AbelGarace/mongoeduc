1401e0f3cbc5949bfcdeb4cd6d93ff830fbde0ad
jrgcubano <jrgcubano@hotmail.com>
2013-11-07 01:30:05 +0100

    Week 2 and Week3 Init

--- m101js/week3/homework/hw3/hw3-2and3-3/blog/users.js
+++ m101js/week3/homework/hw3/hw3-2and3-3/blog/users.js
@@ -28,14 +28,16 @@ function UsersDAO(db) {
             user['email'] = email;
         }
 
-        // TODO: hw2.3
-        // New +
         users.insert(user, function (err, result) {
             "use strict";
-            callback(err, result[0]);
+
+            if (!err) {
+                console.log("Inserted new user");
+                return callback(null, result[0]);
+            }
+
+            return callback(err, null);
         });
-        // Old +
-        //callback(Error("addUser Not Yet Implemented!"), null);
     }
 
     this.validateLogin = function(username, password, callback) {
@@ -66,11 +68,7 @@ function UsersDAO(db) {
             }
         }
 
-        // TODO: hw2.3
-        // New +
-        users.findOne({ '_id' : username}, validateUserDoc(err, user);
-        // Old + 
-        //callback(Error("validateLogin Not Yet Implemented!"), null);
+        users.findOne({ '_id' : username }, validateUserDoc);
     }
 }
 
